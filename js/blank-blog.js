41419bed87c9d6c318c1a4176fe4978482ffc396
roa86 <roa86tmk09@gmail.com>
2019-05-24 13:24:11 +0400

    fix

---
+++ js/blank-blog.js
@@ -0,0 +1,33 @@
+
+
+// Увеличение textarea в зависимости от содержимого
+
+var $heightElem = '',
+    $lineHight = '';
+
+$(document).on('keydown keyup keypress', 'textarea', function() {
+    
+    $heightElem = $(this).outerHeight(); // Получаем высоту элемента
+    $lineHight = Number($(this).css('line-height').substr(0, 2)); // Получаем высоту строки, обрезаем "px" и преобразуем в число
+
+    console.log($lineHight)
+    console.log($heightElem)
+    console.log(this.scrollHeight)
+
+    if (this.scrollHeight > $heightElem) {
+
+        this.style.height = this.scrollHeight + "px";
+    } else if (this.scrollHeight < $heightElem) {
+
+        this.style.height = $heightElem - $lineHight + 'px';
+    };
+    clear($(this));
+});
+
+function clear(elem) {
+
+    if ($(elem).val() >= 1) {
+        
+        elem.style.height = $(elem).css('min-height');
+    }
+}
