7e20b3a6695480aca078bd22d51abb5ab1cd622b
Kira <petrova@background.digital>
2019-11-25 15:05:15 +0400

    add 3red file

---
+++ mainPage/js/link.js
@@ -0,0 +1,32 @@
+'use strict';
+
+let links = document.querySelectorAll('.nav__link');
+
+// links.forEach(link => {
+//     link.addEventListener('click', function () {
+//     links.forEach(link => link.classList.remove('active'));
+//     this.classList.add('active');
+// });
+// });
+
+
+links.forEach(link => {
+    
+
+  link.addEventListener('mouseover', function () {
+    links.forEach(link => link.style.opacity = '.5');
+    
+    this.style.opacity = '1';
+   
+
+  })
+  link.addEventListener('mouseout', function () {
+    
+    links.forEach(link => link.style.opacity = '1');
+
+  })
+  link.addEventListener('click', function () {
+    links.forEach(link => link.classList.remove('active'));
+    this.classList.add('active');
+})
+});
